8926afb94b431362727a57d4e211bf5c238e4ee3
pabitra <89703515+appu900@users.noreply.github.com>
2024-07-05 22:30:06 +0530

    did moodification to marks model

--- src/models/Marks.model.js
+++ src/models/Marks.model.js
@@ -11,12 +11,44 @@ const marksSchema = new mongoose.Schema({
     ref: "Batch",
     required: true,
   },
+  courseId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Course",
+    required: true,
+  },
+  courseName: {
+    type: String,
+    required: true,
+  },
+  markParameters: [
+    {
+      parameterName: {
+        type: String,
+        required: true,
+      },
+      marks: {
+        type: Number,
+        required: true,
+      },
+      maxMarks: {
+        type: Number,
+        required: true,
+      },
+      grade: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
   totalMarks: {
     type: Number,
     required: true,
   },
 });
-   
+
 const Marks = new mongoose.model("Marks", marksSchema);
 export default Marks;
+
+
+
 
