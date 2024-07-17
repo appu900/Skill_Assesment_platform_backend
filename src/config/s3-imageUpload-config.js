c9a77fd28eabcf4ac37f9b484c18a7883940a85e
pabitra <89703515+appu900@users.noreply.github.com>
2024-07-17 18:16:05 +0530

    s3 configuration done

--- src/config/s3-imageUpload-config.js
+++ src/config/s3-imageUpload-config.js
@@ -6,9 +6,34 @@
 import multer from "multer";
 import multerS3 from "multer-s3";
 import aws from "aws-sdk";
+import 'dotenv/config';
+
+// ** aws configuration
 
 aws.config.update({
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   region: process.env.AWS_REGION,
 });
+
+const s3 = new aws.S3();
+
+const upload = multer({
+    storage:multerS3({
+        s3:s3,
+        bucket:process.env.AWS_BUCKET_NAME,
+        acl:'public-read',
+        metadata:(req,file,cb)=>{
+            cb(null,{fieldName:file.fieldname})
+        },
+        key:(req,file,cb)=>{
+            cb(null,Date.now().toString())
+        }
+    })
+});
+
+export default upload;
+
+
+
+
