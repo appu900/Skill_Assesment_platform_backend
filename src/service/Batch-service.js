ebb517e3ad98d15db8cc7014f4f65fdc5e680379
pabitra <89703515+appu900@users.noreply.github.com>
2024-09-19 22:16:54 +0530

    fixed some bug

--- src/service/Batch-service.js
+++ src/service/Batch-service.js
@@ -243,9 +243,7 @@ class BatchService {
       let totalAmount = 0;
       let perStudentCost = 0;
 
-      if(!batch.organizationCorporatePaymentFee){
-        throw new Error("Corporate fee is not assigned by Admin")
-      }
+    
 
       if (batch.schemeType === "Corporate") {
         totalAmount =
